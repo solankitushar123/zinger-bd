4ae85fae6c09470a8c7400cf5c460895e2ffa6fe
Tushar Solanki <solankitushar208@gmail.com>
2026-03-28 12:47:40 +0530

    url corect backend

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use(helmet({
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || 'http://localhost:5173',
+    process.env.CLIENT_URL ||  'https://your-frontend-url.vercel.app','http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
   ],
